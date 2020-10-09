1099cb5c8246a32cb1f252e5e3e776e6f85a1b72
zhangchao <320358015@qq.com>
2020-10-09 20:02:31 +0800

    start

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
+import App from '@/App'
 import Vue from 'vue'
-import App from './App.vue'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el:'#app',
+  render : h => h(App)
+})
